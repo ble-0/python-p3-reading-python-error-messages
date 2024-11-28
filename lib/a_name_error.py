b70dc7cc909e5fe4403e7c172618cc29fe76b76c
bleh <mwendwablessingm@gmail.com>
2024-11-28 12:57:43 +0300

    p error test

--- lib/a_name_error.py
+++ lib/a_name_error.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 
+hello_world=" hello,world" #defining the variable
+
 print(hello_world)
+
--- lib/a_syntax_error.py
+++ lib/a_syntax_error.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
-poor_syntax = 2 * #
+poor_syntax = 2  # This is a placeholder for future multiplication logic
+
+#poor_syntax = 2 * 3  # Uncomment to perform multiplication
+
+
